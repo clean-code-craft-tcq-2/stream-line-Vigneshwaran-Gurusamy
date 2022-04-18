5e9c66fde4eef89f3ab580b585ff612e797350a6
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-18 21:15:46 +0530

    Update BatteryReadingReceiver.cpp

--- BatteryReadingReceiver.cpp
+++ BatteryReadingReceiver.cpp
@@ -1,18 +1,17 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include "test/catch.hpp"
 
 TEST_CASE("Tests to check whether sensor data is read from console") 
 {
   float Temperature[NO_OF_READINGS] = {0};
   float SOC[NO_OF_READINGS] = {0};
   float ChargeRate[NO_OF_READINGS] = {0};
-  BatteryReceiver(Temperature,SOC,ChargeRate);
-  float expectedoutput[3][3] = {{0.9,1.0,1.8}, {53,64,44},{6,44,7}};
-  for(int i=0;i<3;i++)
+  float ComputeDData[10];
+  ComputeDData = BatteryReceiver(Temperature,SOC,ChargeRate);
+  float expectedoutput[10] = {0.000000,1.200000,21.000000,80.000000,0.000000,45.000000,650.000000,702.000000,724.000000};
+  for(int i=0;i<9;i++)
   {
-    REQUIRE(Temperature[i] == expectedoutput[i][0]);
-    REQUIRE(SOC[i] == expectedoutput[i][1]);
-    REQUIRE(ChargeRate[i] == expectedoutput[i][2]);
+    REQUIRE(ComputeDData[i] == expectedoutput[i]);
   }
   
 }
