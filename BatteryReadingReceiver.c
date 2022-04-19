1934accbb7d2bf72f5de351042495a2a578db5aa
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-19 20:15:07 +0530

    Update BatteryReadingReceiver.c

--- BatteryReadingReceiver.c
+++ BatteryReadingReceiver.c
@@ -66,7 +66,6 @@ void ReadBatteryReadingsfromConsole(float* Temperature, float* SOC, float* Charg
     for(int i=0; i<NO_OF_READINGS; i++)
     {
         scanf("%f ,%f ,%f",&ChargeRate[i],&SOC[i],&Temperature[i]);
-        printf("%f,%f,%f",&ChargeRate[i],&SOC[i],&Temperature[i]);
     }
 }
 
