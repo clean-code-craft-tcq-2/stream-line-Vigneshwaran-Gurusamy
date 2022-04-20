b300be3946f8fe1b0f8ceb3baa0ad723e1613384
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-20 11:34:05 +0530

    Update BatteryReadingReceiver.cpp

--- BatteryReadingReceiver.cpp
+++ BatteryReadingReceiver.cpp
@@ -7,15 +7,6 @@ TEST_CASE("Tests to check whether sensor data is read from console")
   float Temperature[NO_OF_READINGS] = {0};
   float SOC[NO_OF_READINGS] = {0};
   float ChargeRate[NO_OF_READINGS] = {0};
-
-  BatteryReceiver(Temperature,SOC,ChargeRate);
-  float expectedoutput[9] = {0.000000,45.000000,21.000000,80.000000,0.000000,1.200000,21.000000,73.000000,74.000000};
-  for(int i=0;i<9;i++)
-  {
-    REQUIRE(ComputeDData[i] == expectedoutput[i]);
-  }
-  
-  
   float Current_MaxValue, Current_MinValue, Current_SMAValue, expectedMaxValue, expectedMinValue, expectedSMAValue;
   
   BatteryReceiver(Temperature,SOC,ChargeRate);
@@ -31,14 +22,13 @@ TEST_CASE("Tests to check whether sensor data is read from console")
   expectedMaxValue = 0.000000;
   expectedMinValue = 45.000000;
   expectedSMAValue = 21.000000;
-  Current_MaxValue = ComputeMax(Temperature[0]);
-  Current_MinValue = ComputeMin(Temperature[0]);
-  Current_SMAValue = ComputeSMA(Temperature[0]);
+  Current_MaxValue = ComputeMax(Temperature);
+  Current_MinValue = ComputeMin(Temperature);
+  Current_SMAValue = ComputeSMA(Temperature);
   REQUIRE(Current_MaxValue == expectedMaxValue);
   REQUIRE(Current_MinValue == expectedMinValue);
   REQUIRE(Current_SMAValue == expectedSMAValue);
     
 }
-  
-}
+ 
 
