ccb11030b7f84c93bcd84b38048fe55794540bc7
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-20 11:34:03 +0530

    Update BatteryReadingReceiver.h

--- BatteryReadingReceiver.h
+++ BatteryReadingReceiver.h
@@ -4,6 +4,6 @@ float ComputeSMA(float data[]);
 float ComputeMax(float data[]);
 float ComputeMin(float data[]);
 
-void ReadBatteryReadingsfromConsole(float* Temperature, float* SOC, float* ChargeRate)
-void BatteryReceiver(float* Temperature, float* SOC, float* ChargeRate)
-void PrintComputedReadingsOnConsole(float *BMSParameter, float MaxValue, float Minvalue, float SMA)
+void ReadBatteryReadingsfromConsole(float* Temperature, float* SOC, float* ChargeRate);
+void BatteryReceiver(float* Temperature, float* SOC, float* ChargeRate);
+void PrintComputedReadingsOnConsole(float *BMSParameter, float MaxValue, float Minvalue, float SMA);
